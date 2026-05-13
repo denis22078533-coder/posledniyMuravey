b41c314ec85d7a3c0653e8d73f7029033e8f3fcb
denis22078533 <denis22078533@gmail.com>
2026-05-13 08:19:58 +0000

    Полная зачистка проекта от poeh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -12,9 +9,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+    <div className="tooltip-provider-fallback">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -23,7 +18,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
+    </div>
   </QueryClientProvider>
 );
 
